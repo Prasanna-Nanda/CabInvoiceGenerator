87061a323e523855270759e38faa3b1241419bc5
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-03-14 20:06:12 +0530

    Added Step2

--- main/CabGenerator.java
+++ main/CabGenerator.java
@@ -1,15 +1,26 @@
 package CabGenerator;
 
+import Ride.Ride;
+
 public class CabGenerator {
     private static final int COST_PER_TIME = 1 ;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final double MINIMUM_FARE=5;
 
 
-    public static double calculatorFare(double distance, int time) {
+    public static double calculateFare(double distance, int time) {
         double totalFare= distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         if(totalFare < MINIMUM_FARE)
             return MINIMUM_FARE;
         return totalFare;
     }
+
+    public static double calculateFare(Ride[] rides) {
+        double totalFare=0;
+        for (Ride ride:rides){
+            this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
+    }
 }
+
